ef3d3e0d24fe7d451b08d233f329fa8b7b0e10e3
Christopher Juwao <njuwao@icloud.com>
2022-05-02 23:53:38 +0200

    Static file error fix

--- whatsAppQuestions/settings.py
+++ whatsAppQuestions/settings.py
@@ -130,6 +130,8 @@ STATIC_URL = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # CORS_ORIGIN_WHITELIST = [
